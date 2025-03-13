4785e780befb4db92a9362a6e5bbf01e50779da6
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-03-13 22:37:20 +0500

    added search history for profile page and for session

--- reviews/views.py
+++ reviews/views.py
@@ -55,29 +55,44 @@ def publisher_edit(request, pk=None):
 
 def book_search(request):
     search_text = request.GET.get("search", "")
+    search_history = request.session.get("search_history", [])
     form = SearchForm(request.GET)
     books = set()
 
-    if form.is_valid() and form.cleaned_data['search']:
-        search = form.cleaned_data['search']
-        search_in = form.cleaned_data['search_in'] or 'title'
-        if search_in == 'title':
+    if form.is_valid() and form.cleaned_data["search"]:
+        search = form.cleaned_data["search"]
+        search_in = form.cleaned_data.get("search_in") or "title"
+        if search_in == "title":
             books = Book.objects.filter(title__icontains=search)
         else:
-            f_name = Contributor.objects.filter(first_names__icontains=search)
+            fname_contributors = Contributor.objects.filter(
+                first_names__icontains=search
+            )
 
-            for contributor in f_name:
+            for contributor in fname_contributors:
                 for book in contributor.book_set.all():
                     books.add(book)
 
-            l_name = Contributor.objects.filter(last_names__icontains=search)
+            lname_contributors = Contributor.objects.filter(
+                last_names__icontains=search
+            )
 
-            for contributor in l_name:
+            for contributor in lname_contributors:
                 for book in contributor.book_set.all():
                     books.add(book)
 
-    return render(request, "reviews/search.html", {"search_text": search_text, 'form': form, "books": books})
+        if request.user.is_authenticated:
+            search_history.append([search_in, search])
+            request.session["search_history"] = search_history
+    elif search_history:
+        initial = dict(search=search_text, search_in=search_history[-1][0])
+        form = SearchForm(initial=initial)
 
+    return render(
+        request,
+        "reviews/search.html",
+        {"form": form, "search_text": search_text, "books": books},
+    )
 
 def book_list(request):
     books = Book.objects.all()
