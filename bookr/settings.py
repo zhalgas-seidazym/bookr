b995c9aaba7cb2c2dd5434307cbc160b164d32af
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-04-04 05:54:27 +0500

    added inclusion tag for reviews

--- bookr/settings.py
+++ bookr/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reviews',
     'bookr_admin',
-    'filter_demo'
+    'filter_demo',
+    'book_management',
 ]
 
 MIDDLEWARE = [
--- bookr/urls.py
+++ bookr/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
 
     path('', include('reviews.urls')),
     path('filter_demo/', include('filter_demo.urls')),
+    path('book_management/', include('book_management.urls')),
 
     path('accounts/profile/', profile, name='profile'),
     path("accounts/", include(("django.contrib.auth.urls", "auth"), namespace="accounts")),
